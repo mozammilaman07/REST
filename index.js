b2d81f6551908b32dd8a851937a9c0e4749c5673
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-03 23:50:49 +0530

    set path of views and public mkdir

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+const port = 8080;
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set(express.static( path.join(__dirname, "public")));
+
+app.listen(port, () => {
+    console.log("Listining to port : 8080");
+});
