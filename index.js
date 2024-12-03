5e38d2896acb7be72bca8687cd6ae533519722b6
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-04 00:32:39 +0530

    updated code for api

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+app.use(express.urlencoded({extended: true}));
+
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.set(express.static( path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.send("server working well!");
+});
 
 app.listen(port, () => {
     console.log("Listining to port : 8080");
